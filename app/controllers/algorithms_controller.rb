b8a1f882eb16547b38d6ba4783afa44955cbd857
Gabriel Henrique <gabrielhfgomes@hotmail.com>
2015-02-11 17:09:00 -0200

    Adicionado verificacao e mensagem de erro para o caso dos campos vazios do calculo de custo

--- app/controllers/algorithms_controller.rb
+++ app/controllers/algorithms_controller.rb
@@ -8,33 +8,32 @@ class AlgorithmsController < ApplicationController
       #This function call the dijkstra algorithm, make a new instance of Algorithm
       #calculate de smaller distance and the path to it.
       def create
-            @algorithm = Algorithm.new(algorithm_params)
-            @algorithm.save
+            @algorithm = Algorithm.create(algorithm_params)
 
-            @algorithm.form_source
-            @algorithm.form_target
-            @algorithm.map_id
+            if @algorithm.save
+                  require_relative '../../lib/dijkstra.rb'
+                  @routes = Route.where(map_id: @algorithm.map_id)
 
-            require_relative '../../lib/dijkstra.rb'
-            @routes = Route.where(map_id: @algorithm.map_id)
+                  @graph = Graph.new
+                  (1..6).each {|node| @graph.push node }
 
-            @graph = Graph.new
-            (1..6).each {|node| @graph.push node }
+                  @routes.each do |route|
+                        @graph.connect_mutually route.source.to_i, route.target.to_i, route.distance.to_i
+                  end
 
-            @routes.each do |route|
-                  @graph.connect_mutually route.source.to_i, route.target.to_i, route.distance.to_i
+                  @graph
+                  @graph.length_between(@algorithm.form_source.to_i, @algorithm.form_target.to_i)
+                  @graph.neighbors(@algorithm.form_source.to_i)
+
+
+                  minimum_distance = @graph.dijkstra(@algorithm.form_source.to_i, @algorithm.form_target.to_i)[:distance]
+
+                  @algorithm.form_total = (minimum_distance/@algorithm.form_autonomy) * @algorithm.form_cost
+            else
+                  render 'errors/routesError'
             end
-
-            @graph
-            @graph.length_between(@algorithm.form_source.to_i, @algorithm.form_target.to_i)
-            @graph.neighbors(@algorithm.form_source.to_i)
-
-
-            minimum_distance = @graph.dijkstra(@algorithm.form_source.to_i, @algorithm.form_target.to_i)[:distance]
-
-            @algorithm.form_total = (minimum_distance/@algorithm.form_autonomy) * @algorithm.form_cost
-
       end
+
 
       private def algorithm_params
             params.require(:algorithm).permit(:map_id, :form_source, :form_target, :form_autonomy, :form_cost)
